0e0b50249551d4151fc0cf4ec52a3115d6134373
Tanya Olieinykova <dp210795dtj1@gmail.com>
2024-05-19 23:59:37 +0300

    add tasks.js

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+    let makeNumberContent = Number.parseFloat (content);
+    let makeNumberPadding = Number.parseFloat (padding);
+    let makeNumberBorder = Number.parseFloat (border);
+
+    return makeNumberContent + 2 * (makeNumberPadding + makeNumberBorder)
+
+}
+console.log(getElementWidth("50px", "8px", "4px")); 
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 
